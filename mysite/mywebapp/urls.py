44a2f68e427b8c0b06a4daab811d7d8f9121a549
Micah Guttman <memtech3@gmail.com>
2020-01-29 12:52:30 -0600

    attempting login page

--- mysite/mywebapp/urls.py
+++ mysite/mywebapp/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index')
+    url('^$', views.index, name='index'),
+    url('login/', views.login, name='login'),
+    url('flables/', views.flables, name='flables'),
+
 ]
--- mysite/mywebapp/views.py
+++ mysite/mywebapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
+
 def index(request):
     return render(request, 'mywebapp/home.html')
-# Create your views here.
+
+def login(request):
+    return render(request, 'mywebapp/login.html')
+
+def flables(request):
+    return render(request, 'mywebapp/floating-labels.html')
