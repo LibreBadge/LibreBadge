502f502f815251b0d2a96d1fd7efe74ce908286c
Micah Guttman <memtech3@gmail.com>
2020-01-30 12:06:33 -0600

    login form renders properly

--- mysite/mywebapp/views.py
+++ mysite/mywebapp/views.py
@@ -24,7 +24,21 @@ def logout_request(request):
     return redirect("")
 
 def login_request(request):
-    form = AuthenticationForm(),
-    return render(request,
-                    "mywebapp/login.html",
-                    context={"form":form} )
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "mywebapp/login.html",
+                    context={"form":form})
