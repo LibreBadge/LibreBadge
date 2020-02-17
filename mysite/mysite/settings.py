4c2f813cff1260cbfa177a4e67532ea486d1a78b
Micah Guttman <memtech3@gmail.com>
2020-02-16 20:38:23 -0600

    added cardholders db to settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'cardholders': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'cardholders.sqlite3'),
     }
 }
 
