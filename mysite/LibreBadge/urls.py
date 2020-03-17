38c0a419c9b9aeebbd77a3966864dec2344d1b40
Micah Guttman <memtech3@gmail.com>
2020-03-17 13:50:03 -0500

    added media uploads

--- mysite/LibreBadge/urls.py
+++ mysite/LibreBadge/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -12,3 +14,6 @@ urlpatterns = [
     url("databaseTest/", views.databaseTest, name="databaseTest"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
