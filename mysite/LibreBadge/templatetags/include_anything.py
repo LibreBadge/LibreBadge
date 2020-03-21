92286a80fc8b509de4c03262ebc07cf06e2384ec
Micah Guttman <memtech3@gmail.com>
2020-03-20 19:41:30 -0500

    inline svgs work

--- mysite/LibreBadge/templatetags/include_anything.py
+++ mysite/LibreBadge/templatetags/include_anything.py
@@ -1,6 +1,8 @@
 from django import template
+from django.utils.safestring import mark_safe
 register = template.Library()
  
 @register.simple_tag
 def include_anything(file_name):
-    return open(file_name).read()
+    file = open (file_name).read()
+    return mark_safe(file)
