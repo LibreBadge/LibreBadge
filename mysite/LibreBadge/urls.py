96dc1dab9f8ec09f4be680f32eca0f5d4027fc2c
Micah Guttman <memtech3@gmail.com>
2020-03-29 23:37:34 -0500

    removed old cookietemplate. RIP Kai's code. It was true art
    @Quantaly

--- mysite/LibreBadge/urls.py
+++ mysite/LibreBadge/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     url('^$', views.index, name='index'),
     url('login/', views.login_request, name='login'),
     url("logout/", views.logout_request, name="logout"),
-    url("cookietemplate/", views.cookietemplate, name="cookietemplate"),
     url(r'^production/(?P<slug>[-\w]+)/$', views.production, name="production"),
     url("databaseTest/", views.databaseTest, name="databaseTest"),
 
--- mysite/LibreBadge/views/views.py
+++ mysite/LibreBadge/views/views.py
@@ -11,11 +11,6 @@ def production(request, slug):
     context = {"BadgeTemplate":BadgeTemplate.objects.all,"AlertMessage":AlertMessage.objects.all,"slug":slug})
 
 @login_required
-def cookietemplate(request):
-    return render(request, 'LibreBadge/cookietemplate.html',
-    context = {"AlertMessage":AlertMessage.objects.all})
-
-@login_required
 def databaseTest(request):
     return render(request, 'LibreBadge/databaseTest.html',
     context = {"AlertMessage":AlertMessage.objects.all})
