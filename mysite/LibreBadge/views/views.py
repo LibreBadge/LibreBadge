0434c0dd9dc01d1c21ae3eb451fdb7bb1bd782de
Micah Guttman <memtech3@gmail.com>
2020-04-29 22:30:54 -0500

    basic sql query works, now time to connect it to the view
    #21

--- mysite/LibreBadge/views/views.py
+++ mysite/LibreBadge/views/views.py
@@ -1,4 +1,5 @@
 from .imports import *
+from django.db import connections
 #Put all views that don't belong elsewere here
 @login_required
 def index(request):
@@ -12,5 +13,10 @@ def production(request, slug):
 
 @login_required
 def databaseTest(request):
+    with connections['cardholders'].cursor() as cursor:
+        cursor.execute("SELECT * FROM cardholders WHERE id = 1")
+        row = cursor.fetchall()
+        print (row)
+        cursor.close()
     return render(request, 'LibreBadge/databaseTest.html',
     context = {"AlertMessage":AlertMessage.objects.all})
