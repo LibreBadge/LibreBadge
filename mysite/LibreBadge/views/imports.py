f60751d0c0a6fafb083c59dba10679beebbb5b34
Micah Guttman <memtech3@gmail.com>
2020-05-05 13:00:09 -0500

    moved authentication imports to authentication file

--- mysite/LibreBadge/views/imports.py
+++ mysite/LibreBadge/views/imports.py
@@ -1,8 +1,6 @@
 #put all imports for views here
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from ..models import AlertMessage, BadgeTemplate
