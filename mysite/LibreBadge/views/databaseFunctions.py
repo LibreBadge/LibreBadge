5d192df244d6bb442af73b109bf78612904e8cdf
Micah Guttman <memtech3@gmail.com>
2020-05-12 23:39:06 -0500

    removed unused function

--- mysite/LibreBadge/views/databaseFunctions.py
+++ mysite/LibreBadge/views/databaseFunctions.py
@@ -1,25 +1,4 @@
 from django.db import connections
-from collections import namedtuple
-
-def namedtuplefetchall(cursor):
-    "Return all rows from a cursor as a namedtuple"
-    desc = cursor.description
-    nt_result = namedtuple('Result', [col[0] for col in desc])
-    return [nt_result(*row) for row in cursor.fetchall()]
-
-def select(db, table, field, value):
-    with connections[db].cursor() as cursor:
-                qry = "SELECT * FROM " + table + " WHERE " + field + " = %s"
-                cursor.execute(qry,[value])
-                return namedtuplefetchall(cursor)
-                cursor.close()
-
-def selectStartingWith(db, table, field, value):
-    with connections[db].cursor() as cursor:
-                qry = "SELECT * FROM " + table + " WHERE " + field + " LIKE %s%%"
-                cursor.execute(qry,[value])
-                return namedtuplefetchall(cursor)
-                cursor.close()
 
 def formQuery(db, columns, table, values):
     columnsComma = ', '.join(columns)
