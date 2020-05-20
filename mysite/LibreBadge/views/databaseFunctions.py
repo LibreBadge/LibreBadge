8040cd471b3fab272c9309551c0e43e6bcefe886
Micah Guttman <memtech3@gmail.com>
2020-05-19 22:23:43 -0500

    made first parameter of query select, prepping for svg templating finally

--- mysite/LibreBadge/views/databaseFunctions.py
+++ mysite/LibreBadge/views/databaseFunctions.py
@@ -1,21 +1,21 @@
 from django.db import connections
 
-def select(db, columns, table, field, value):
-    columnsComma = ', '.join(columns)
-    with connections[db].cursor() as cursor:
-                qry = "SELECT "+ columnsComma + " FROM " + table + " WHERE " + field + " = %"
-                cursor.execute(qry,[value])
-                return cursor.fetchall()
-                cursor.close()
-
 def formQuery(db, columns, table, values):
     columnsComma = ', '.join(columns)
     valuesLike = [sub + '%' for sub in values]
+    like = True
+    if values[0] is not '':
+        like = False
+        del valuesLike[0];
+        valuesLike.insert(0,(values[0]))
     with connections[db].cursor() as cursor:
                 qry = "SELECT "+ columnsComma + " FROM " + table + " "
                 for i, x in enumerate(columns):
                     if not i:
-                        qry += "WHERE " + x + " LIKE %s "
+                        if like is False:
+                            qry += "WHERE " + x + " = %s "
+                        else:
+                            qry += "WHERE " + x + " LIKE %s "
                     else:
                         qry += "AND " + x + " LIKE %s "
                 cursor.execute(qry,valuesLike)
