a92fc567a841fb1673f51bc17b93a846527ff51d
Micah Guttman <memtech3@gmail.com>
2020-05-27 22:54:48 -0500

    cut the crap out of the insert function

--- mysite/LibreBadge/views/databaseFunctions.py
+++ mysite/LibreBadge/views/databaseFunctions.py
@@ -23,23 +23,13 @@ def formQuery(db, columns, table, values):
                 cursor.close()
 
 def formCreate(db, columns, table, values):
-    columnsComma = ', '.join(columns)
-    like = True
     with connections[db].cursor() as cursor:
-                qry = "SELECT "+ columnsComma + " FROM " + table + " "
-                for i, x in enumerate(columns):
-                    if not i:
-                        if like is False:
-                            qry += "WHERE " + x + " = %s "
-                        else:
-                            qry += "WHERE " + x + " LIKE %s "
-                    else:
-                        qry += "AND " + x + " LIKE %s "
-                cursor.execute(qry,valuesLike)
+                qry = "SELECT "+ columns[0] + " FROM " + table + " WHERE " + columns[0] + " = %s "
+                cursor.execute(qry,values[0])
                 row = cursor.fetchall()
                 cursor.close()
     if row is not None:
-        raise Exception("Record with primary key already exists")
+        raise Exception("Record with the same primary key already exists")
     with connections[db].cursor() as cursor:
                 qry = "INSERT INTO " + table " ("+ columnsComma + ") Values (" + values + ");"
                 cursor.execute(qry,valuesLike)
