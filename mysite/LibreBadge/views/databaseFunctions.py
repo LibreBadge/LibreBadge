e844212074b24c0c873623cf56707ff415483702
Micah Guttman <memtech3@gmail.com>
2020-05-27 22:46:39 -0500

    create function created, now to test

--- mysite/LibreBadge/views/databaseFunctions.py
+++ mysite/LibreBadge/views/databaseFunctions.py
@@ -21,3 +21,27 @@ def formQuery(db, columns, table, values):
                 cursor.execute(qry,valuesLike)
                 return cursor.fetchall()
                 cursor.close()
+
+def formCreate(db, columns, table, values):
+    columnsComma = ', '.join(columns)
+    like = True
+    with connections[db].cursor() as cursor:
+                qry = "SELECT "+ columnsComma + " FROM " + table + " "
+                for i, x in enumerate(columns):
+                    if not i:
+                        if like is False:
+                            qry += "WHERE " + x + " = %s "
+                        else:
+                            qry += "WHERE " + x + " LIKE %s "
+                    else:
+                        qry += "AND " + x + " LIKE %s "
+                cursor.execute(qry,valuesLike)
+                row = cursor.fetchall()
+                cursor.close()
+    if row is not None:
+        raise Exception("Record with primary key already exists")
+    with connections[db].cursor() as cursor:
+                qry = "INSERT INTO " + table " ("+ columnsComma + ") Values (" + values + ");"
+                cursor.execute(qry,valuesLike)
+                return cursor.fetchall()
+                cursor.close()
