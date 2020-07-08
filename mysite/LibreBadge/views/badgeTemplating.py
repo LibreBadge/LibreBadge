96e0f4f64bb929081ed99cf8b59d5f1e7d54a144
Micah Guttman <memtech3@gmail.com>
2020-07-08 00:09:08 -0500

    more noodling

--- mysite/LibreBadge/views/badgeTemplating.py
+++ mysite/LibreBadge/views/badgeTemplating.py
@@ -1,20 +1,19 @@
 from ..models import BadgeTemplate
 import .svglue
+# pass the str(tmp) to the django template in veiw context variable. Make sure to mark safe so that it doesn't get escaped.
 
-def badgeTemplatingEngine (instance)
+def badgeTemplatingEngine2 (instance, values):
+    columns = list()
+    values = list()
+    for BadgeTemplateCardConfig in BadgeTemplateConfigFile['Card']:
+        columns.append(BadgeTemplateCardConfig['DatabaseColumn'])
+    rows = formQuery('cardholders', columns, 'cardholders', values)
     tmp = svglue.load(file=instance.Template.url)
-    for BadgeTemplateFormConfig in instance.BadgeTemplateConfigFile['Card']:
+    for column in columns:
         if BadgeTemplateFormConfig['type'] == 'text':
-            with connections[db].cursor() as cursor:
-                qry = "SELECT" + 'databaseFields' "WHERE" + BadgeTemplateConfigFile['FormFields'].0.'databaseField' = rows[0]
-                cursor.execute(qry,valuesLike)
-                return cursor.fetchall()
-                cursor.close()
-            tmp.set_text(BadgeTemplateFormConfig['SVGTemplateID'], )
+            tmp.set_text(BadgeTemplateFormConfig['SVGTemplateID'], 'text')
         if 'type' =='image/png':
             tmp.set_image('pink-box', file='hello.png', mimetype='image/png')
-        if 'type' == 'barcode/REGEX HERE'
+        if 'type' == 'barcode/REGEX HERE':
             tmp.set_svg('yellow-box', src='OUTPUTFROMBARCODEGENERATORLIBRARY')
     return str(tmp)
-
-# pass the str(tmp) to the django template in veiw context variable. Make sure to mark safe so that it doesn't get escaped.
