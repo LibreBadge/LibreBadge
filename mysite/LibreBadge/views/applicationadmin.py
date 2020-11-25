11688592186e7656d3206ec653d35ca80a2a429a
Micah Guttman <memtech3@gmail.com>
2020-11-24 23:53:39 -0600

    itemadmin can now get titles of AdminItems from slug

--- mysite/LibreBadge/views/applicationadmin.py
+++ mysite/LibreBadge/views/applicationadmin.py
@@ -7,8 +7,8 @@ from django.http import Http404
 import urllib.parse
 
 AdminItems = [
-    {"model":"BadgeTemplates", "title":"Badge Templates", "description":"Add or modify badge templates", "icon":"fas fa-id-badge", "url":"LibreBadge:itemadmin"},
-    {"model":"AlertMessages", "title":"Alert Messages", "description":"Add or modify alert messages", "icon":"fas fa-exclamation-triangle", "url":"LibreBadge:itemadmin"}
+    {"model":"BadgeTemplate", "title":"Badge Templates", "description":"Add or modify badge templates", "icon":"fas fa-id-badge", "url":"LibreBadge:itemadmin"},
+    {"model":"AlertMessage", "title":"Alert Messages", "description":"Add or modify alert messages", "icon":"fas fa-exclamation-triangle", "url":"LibreBadge:itemadmin"}
 ]
 
 @login_required
@@ -22,9 +22,13 @@ def applicationadmin(request):
 
 @login_required
 def itemadmin(request,slug):
+    AdminItem = {}
+    for item in AdminItems:
+        if item.get('model') == slug:
+            AdminItem = item
     try:
         True
     except:
         raise Http404("Badge Template Doesn't Exist")
     return render(request, 'LibreBadge/applicationadmin/itemadmin.html',
-    context = {"BadgeTemplate":BadgeTemplate.objects.all,"AlertMessage":AlertMessage.objects.all,"title":slug})
+    context = {"results":BadgeTemplate.objects.all,"AlertMessage":AlertMessage.objects.all,"title":AdminItem.get('title')})
