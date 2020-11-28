73a50b2531df52b163578946d6f4a2e571a999ce
Micah Guttman <memtech3@gmail.com>
2020-11-28 17:52:56 -0600

    succesfully iterated over field value AND type in itemadmin template

--- mysite/LibreBadge/views/applicationadmin.py
+++ mysite/LibreBadge/views/applicationadmin.py
@@ -29,21 +29,18 @@ def itemadmin(request,slug):
     fields = []
     for field in eval(AdminItem.get('model') + '._meta.get_fields()'):
         fields.append(field.name)
-    fieldTypes = []
-    values = []
-    for i, value in enumerate(eval(AdminItem.get('model') + '.objects.values()'), start=0):
-        fieldTypes.append([])
+    results = []
+    for value in eval(AdminItem.get('model') + '.objects.values()'):
+        fieldTypes2 = []
         for field in eval(AdminItem.get('model') + '._meta.get_fields()'):
-            fieldTypes[i].append(field.get_internal_type())
-        fieldTypes[i] = tuple(fieldTypes[i])
-        values.append([])
+            fieldTypes2.append(field.get_internal_type())
+        values2 = []
         for item in list(value.values()):
-            values[i].append(item)
-        values[i] = tuple(values[i])
-        
+            values2.append(item)
+        results.append(list(zip(values2, fieldTypes2)))
     try:
         True
     except:
         raise Http404("Badge Template Doesn't Exist")
     return render(request, 'LibreBadge/applicationadmin/itemadmin.html',
-    context = {"fields":fields, "fieldTypes":fieldTypes, "results":dict(zip(values, fieldTypes)), "BadgeTemplate":BadgeTemplate.objects.all, "AlertMessage":AlertMessage.objects.all,"title":AdminItem.get('title')})
+    context = {"fields":fields, "results":results, "BadgeTemplate":BadgeTemplate.objects.all, "AlertMessage":AlertMessage.objects.all,"title":AdminItem.get('title')})
